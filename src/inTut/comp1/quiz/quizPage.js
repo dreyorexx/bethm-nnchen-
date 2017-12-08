bf6cd5d287f14a3294cb569f8c1924edaba09341
dcapperx <dreyorexx@gmail.com>
2017-12-08 08:29:11 +0800

    Updated Quiz

--- src/inTut/comp1/quiz/quizPage.js
+++ src/inTut/comp1/quiz/quizPage.js
@@ -3,6 +3,8 @@ import update from 'react-addons-update';
 import quizQuestions from '../../../api/quizQuestions';
 import Quiz from '../../../Components/quiz';
 import Result from '../../../Components/Result';
+import { Header, List, Menu, Segment, Container, Dropdown, Grid, Button, Icon } from 'semantic-ui-react'
+
 
 class quizPage extends Component {
 
@@ -122,14 +124,108 @@ class quizPage extends Component {
     );
   }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Test Quiz</h2>
-        </div>
-        {this.state.result ? this.renderResult() : this.renderQuiz()}
-      </div>
+      <Segment
+        style={{ backgroundColor: '#2e2f30'}}
+        >
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Segment
+              textAlign='center'
+              style={{ maxHeight: 70, backgroundColor: 'black' }}
+              vertical
+              >
+                <Container>
+                  <Menu inverted fixed='top'>
+                    <Menu.Item as='a'><a href="/Header">Dashboard</a></Menu.Item>
+                    <Menu.Item as='a' active><a href="/Tutorial">Tutorial</a></Menu.Item>
+                    <Menu.Item as='a'><a href="/Doc">Documentation</a></Menu.Item>
+
+                    <Menu.Menu position='right'>
+                      <Dropdown item text='Drey'>
+                        <Dropdown.Menu>
+                          <Dropdown.Item>Profile</Dropdown.Item>
+                          <Dropdown.Item>Log Out</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </Menu.Menu>
+                  </Menu>
+                </Container>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <Segment
+                style={{maxMargin: 60, backgroundColor: '#2e2f30', paddingLeft: 70}}
+                >
+                  <Menu pointing vertical>
+                    <Menu.Item as='a' ><a href="/Obj">Learning Objectives</a>
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    <Menu.Header>Component 1</Menu.Header>
+                    <Menu.Menu>
+                      <Menu.Item name='Video' active={activeItem === 'video1'} onClick={this.handleItemClick} ><a href="/video">Video</a></Menu.Item>
+                      <Menu.Item name='Quiz' active={activeItem === 'quiz1'} onClick={this.handleItemClick} active/>
+                    </Menu.Menu>
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    <Menu.Header>Component 2</Menu.Header>
+                    <Menu.Menu>
+                      <Menu.Item name='Video' active={activeItem === 'video2'} onClick={this.handleItemClick} />
+                      <Menu.Item name='Quiz' active={activeItem === 'quiz2'} onClick={this.handleItemClick} />
+                    </Menu.Menu>
+                  </Menu.Item>
+
+                  <Menu.Item>
+                    <Menu.Header>Practise</Menu.Header>
+                    <Menu.Menu>
+                      <Menu.Item name='Practical 1' active={activeItem === 'practical1'} onClick={this.handleItemClick} />
+                      <Menu.Item name='Practical 2' active={activeItem === 'practical2'} onClick={this.handleItemClick} />
+                    </Menu.Menu>
+                  </Menu.Item>
+                </Menu>
+              </Segment>
+            </Grid.Column>
+
+            <Grid.Column width={11}>
+              <Segment
+                textAlign='left'
+                style={{ minHeight: 70,  backgroundColor: '#2e2f30', marginTop: -20 }}
+                >
+
+                  <Container>
+                    <Segment
+                      style={{ color: 'white', backgroundColor: '#2e2f30' }}
+                      >
+                    <Grid.Row>
+                    <h2 >Test Quiz</h2>
+                    </Grid.Row>
+
+                    <Grid.Row>
+                      {this.state.result ? this.renderResult() : this.renderQuiz()}
+                    </Grid.Row>
+                    </Segment>
+
+                    <Grid.Row>
+                        <Button style ={{marginLeft:"1em"}} color='brown' content='Back to Lecture' icon='left arrow' size='small' labelPosition='left' />
+                        <Button style={{float: "right", marginLeft:"10em"}} size='small' content='Next Lecture' color='blue' icon='right arrow' labelPosition='right' />
+
+                    </Grid.Row>
+                  </Container>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+    </Grid>
+  </Segment>
     );
   }
 
